42bbfd84d7bfab868e3b62e117298580fefb7d65
affandhanse <160419467+affandhanse@users.noreply.github.com>
2025-02-27 14:54:21 +0530

    Task 3 for Assignment Day 3
    task1 and 2 are correlated and hence uploaded in single file asssignment1day3, task 3 is uploaded as a seperate file

---
+++ Assignment2Day3/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Text.RegularExpressions;namespace Task3{    internal class Program    {        public static Boolean validatePassword(String password)        {            if (password.Length < 6)            {                Console.WriteLine("Password must be at least 6 characters long.");                return false;            }            if (!Regex.IsMatch(password, "[A-Z]"))            {                Console.WriteLine("Password must contain at least one uppercase letter.");                return false;            }            if (!Regex.IsMatch(password, "\\d"))            {                Console.WriteLine("Password must contain at least one digit.");                return false;
+
+            }            Console.WriteLine("Password is valid.");            return true;        }        static void Main(string[] args)        {            Console.WriteLine("Enter Password::");            string pass = Console.ReadLine();            validatePassword(pass);
+
+        }    }}
