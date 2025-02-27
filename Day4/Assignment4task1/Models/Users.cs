2db81d3004b4a6b9922bc4c47363da9d6612538f
affandhanse <160419467+affandhanse@users.noreply.github.com>
2025-02-27 15:13:06 +0530

    Assignment day 4 part1

---
+++ Day4/Assignment4task1/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4task1.Models
+{
+    internal class Users
+    {
+        static int num = 0;
+        public Users()
+        {
+            num = num + 1;
+            Console.WriteLine($"User has logged In.........");
+        }
+        public static void UserCount()
+        {
+            Console.WriteLine($"User Count is :: {num}");
+        }
+    }
+}
---
+++ Day4/Assignment4task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Assignment4task1.Models;
+
+namespace Assignment4task1
+{
+    internal class Program:Users
+    {
+        static void Main(string[] args)
+        {
+            Users log1 = new Users();
+            Users log2 = new Users();
+            Users log3 = new Users();
+            Users log4 = new Users();
+            Users log5 = new Users();
+
+            Users.UserCount();
+
+        }
+    }
+}
