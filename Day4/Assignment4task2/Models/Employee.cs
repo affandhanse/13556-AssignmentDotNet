53e38cd003cea73f2f3fd3500b438c8c3823539d
affandhanse <160419467+affandhanse@users.noreply.github.com>
2025-02-27 15:55:57 +0530

    Employee System assignment 4 task 2

---
+++ Day4/Assignment4task2/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4task2.Models
+{
+    internal class Employee
+    {
+        public String Name { get; set; }
+        public double Salary { get; set; }
+
+         public  Employee(string name,  double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+         public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Employee Details are ::");
+            Console.WriteLine($"Employee Name :: {Name}\t Employee Salary :: {Salary}\n");
+        }
+
+    }
+}
---
+++ Day4/Assignment4task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Assignment4task2.Models;
+
+namespace Assignment4task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmp = new Employee("affan", 20000);
+            newEmp.DisplayDetails();
+
+            Manager newManager = new Manager("Atharv", 2500000, 200000);
+            newManager.DisplayDetails();
+
+
+            Employee newEmp2 = new Manager("Pranit", 200000, 10000);
+            newEmp2.DisplayDetails();
+
+            //Manager newManager2 = (Manager)new Employee("Pratik", 200000);
+            //newManager2.DisplayDetails();
+        }
+
+    }
+}
