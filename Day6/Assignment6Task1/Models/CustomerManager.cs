fe452b5ba2e6bf25abadb9dbd07473f4ff6c17b6
affandhanse <160419467+affandhanse@users.noreply.github.com>
2025-03-03 10:09:41 +0530

    Day6 task1

---
+++ Day6/Assignment6Task1/Models/CustomerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6Task1.Models
+{
+    internal class CustomerManager
+    {
+            public static void Add(Queue<string> CustomerLine, string customerName)
+            {
+                CustomerLine.Enqueue(customerName);
+                Console.WriteLine($"{customerName} has been added.");
+            }
+
+            public static void ServeCus(Queue<string> CustomerLine)
+            {
+                if (CustomerLine.Count > 0)
+                {
+                    string served = CustomerLine.Dequeue();
+                    Console.WriteLine($"{served} - has been served.");
+                }
+                else
+                {
+                    Console.WriteLine("No customer in line.");
+                }
+            }
+
+            public static string NextCus(Queue<string> CustomerLine)
+            {
+                if (CustomerLine.Count > 0)
+                {
+                    return $"Next customer is: {CustomerLine.Peek()}";
+                }
+                else
+                {
+                    return "No customer in line.";
+                }
+            }
+        }
+    }
---
+++ Day6/Assignment6Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Assignment6Task1.Models;
+
+namespace Assignment6Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> CustomerLine = new Queue<string>();
+            CustomerLine.Enqueue("Pranit");
+            CustomerLine.Enqueue("Atharva");
+            CustomerLine.Enqueue("Pratik");
+            CustomerLine.Enqueue("Sanchita");
+
+            Console.WriteLine("Enter Customer Name:");
+            string customerName = Console.ReadLine();
+            CustomerManager.Add(CustomerLine, customerName);
+
+            Console.WriteLine("Do you want to serve a customer? (yes/no)");
+            string serveResponse = Console.ReadLine();
+            if (serveResponse.ToLower() == "yes")
+            {
+                CustomerManager.ServeCus(CustomerLine);
+            }
+
+            Console.WriteLine(CustomerManager.NextCus(CustomerLine));
+        }
+    }
+}
